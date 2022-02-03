4292bf5f0c7bf381d3d16e10002a15880df8d68f
joennespreuwers <spreuwersjoenne@gmail.com>
2022-02-03 14:37:44 +0100

    :sparkles: New exercise

--- tkinter_buttons/H2.3_Buttons.py
+++ tkinter_buttons/H2.3_Buttons.py
@@ -1,7 +1,11 @@
+from cgitb import reset
+from glob import glob
+from ipaddress import collapse_addresses
+import os, time
 from tkinter import *
 from tkinter import messagebox
 
-from attr import field
+import black
 
 # LIJST VARIABELEN
 scherm = Tk()
@@ -18,7 +22,7 @@ achtergrondkleur = "#387389"
 naam = "Ik ben een knoppeke"
 achtergrond = "#25b9f0"
 voorgrond = "white"
-image = PhotoImage(file='./image.png')
+image = PhotoImage(file="./image.png")
 
 # FUNCTIES
 def geklikt():
@@ -40,30 +44,58 @@ def foutmelding():
 def afsluiten():
     if messagebox.askyesno("KIEZEN", "Wil je de pc afsluiten?"):
         messagebox.showinfo("AFSLUITEN", "Klik nog eens")
+        time.sleep(3)
+        os.system("shutdown -s")
     else:
         messagebox.showinfo("AFSLUITEN", "Goed gekozen")
 
 
 def kleur_veranderen():
     global knop6
-    knop6.configure(bg="Black")
+    knop6.configure(bg="white")
+    knop6.configure(fg="black")
 
 
 def onbruikbaar():
     global knop7
     knop7.configure(state="disabled")
+    knop7.configure(bg="white")
+
+
+def reset():
+    global knop7
+    knop7.configure(state="active")
 
 
 # AANMAKEN KNOPPPEN
 
 knop1 = Button(scherm, text=naam, command=informatie)
+
 knop2 = Button(
     scherm, text="informatie", bg="red", overrelief="sunken", command=informatie
 )
 knop3 = Button(
     scherm, text="red alert", fg="green", relief="sunken", command=waarschuwing
 )
-knop4 = Button(scherm, image=PhotoImage(file="./image.png"))
+knop4 = Button(scherm, text="image")
+
+knop5 = Button(scherm, text="Shut down", width=20, height=10, command=afsluiten)
+
+knop6 = Button(scherm, text="knop6", fg="white", bg="black", command=kleur_veranderen)
+
+knop7 = Button(
+    scherm,
+    text="knop7",
+    fg="white",
+    bg="black",
+    width=5,
+    height=10,
+    padx=10,
+    pady=10,
+    command=onbruikbaar,
+)
+
+knop8 = Button(scherm, text="reset", width=1, height=10, command=reset)
 
 # INSTELLEN EIGENSCHAPPEN SCHERM
 
@@ -77,6 +109,10 @@ knop1.grid()
 knop2.grid()
 knop3.grid()
 knop4.grid()
+knop5.grid()
+knop6.grid()
+knop7.grid()
+knop8.grid()
 
 
 # HOOFDPROGRAMMA
